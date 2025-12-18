d5fcb6ac24078dc116812dc915a4131aad09a243
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-12-18 20:09:24 +0100

    Javascript advanced tasks

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,28 @@
+function countPrimeNumbers() {
+  let count = 0;
+
+  for (let n = 2; n <= 100; n += 1) {
+    let isPrime = true;
+
+    for (let i = 2; i <= Math.sqrt(n); i += 1) {
+      if (n % i === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) count += 1;
+  }
+
+  return count;
+}
+
+const start = performance.now();
+for (let i = 0; i < 100; i += 1) {
+  countPrimeNumbers();
+}
+const end = performance.now();
+
+console.log(
+  `Execution time of printing countPrimeNumbers was ${end - start} milliseconds.`
+);
